83ee617534cfb5271426d9c8430b26c4ebb5f3b3
habib27akbar <akbarca94@gmail.com>
2021-01-23 11:42:08 +0700

    merubah app.js

--- App.js
+++ App.js
@@ -1,115 +1,82 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
-import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
-
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-const App: () => React$Node = () => {
+import React, { Component } from 'react';
+import {Image, Text, View} from 'react-native';
+const App = () => {
   return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Langkah Pertama</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this   
-                screen and then come back to see your edits.
-                Kamu pasti bisa
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
+    <View>
+      
+      <Profile/>
+      <ProfileKucing/>
+     
+    </View>
+    
   );
 };
 
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
+const Profile = () => {
+  return (
+    <View>
+        <Image 
+        source={{ uri: 'http://placeimg.com/640/480/tech' }} 
+        style={{ 
+          width: 200, 
+          height: 200,
+          marginLeft:20
+          }} />
+        <Text 
+          style={{ 
+                  backgroundColor: '#3498db', 
+                  color:'white', width: 200, 
+                  fontSize:19,
+                  marginLeft:20
+                }}>
+          Habib Akbar Ramdhani
+        </Text>
+        <Text 
+          style={{ 
+            backgroundColor: '#3498db', 
+            color:'white', 
+            width: 200, 
+            fontSize:20,
+            marginLeft:20 
+            }}>
+          26 Tahun
+        </Text>
+
+        </View>
+  );
+};
+
+class ProfileKucing extends Component{
+  render(){
+    return (
+      <View>
+         <Image 
+         source={{ uri: 'http://placeimg.com/640/480/animals' }} 
+         style={{ width: 100, 
+                  height: 100,
+                  marginTop: 10
+                  }} />
+         <Text 
+            style={{ 
+                    backgroundColor: '#666888', 
+                    color:'white', width: 100, 
+                    fontSize:20
+                  }}>
+            Bambang
+          </Text>
+          <Text 
+            style={{ 
+              backgroundColor: '#666888', 
+              color:'white', 
+              width: 100, 
+              fontSize:20 
+              }}>
+            21 Tahun
+          </Text>
+
+      </View>
+    )
+  }
+}
 
 export default App;
