d6db6afeb2e4392f148d1ea8e917be6d38403918
habib27akbar <akbarca94@gmail.com>
2021-01-30 21:47:56 +0700

    belajar Vanilla API , dan SVG

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import {Image, StyleSheet, Text, View} from 'react-native';
 //import Profile from './Profile';
-import FlexBox from './pages/FlexBox';
+//import FlexBox from './pages/FlexBox';
 //import PropsDinamis from './pages/PropsDinamis';
-import StateDinamis from './pages/StateDinamis';
+//import StateDinamis from './pages/StateDinamis';
+//import RnSvg from './pages/RnSvg';
+import CallAPIVanilla from './pages/CallAPIVanilla';
 const App = () => {
   return (
     <View>
-      
+      {/* <Profile/> */}
       {/* <FlexBox/> */}
       {/* <PropsDinamis/> */}
-      <StateDinamis/>
-     
+      {/* <StateDinamis/> */}
+      {/* <RnSvg/> */}
+     <CallAPIVanilla/>
     </View>
     
   );
