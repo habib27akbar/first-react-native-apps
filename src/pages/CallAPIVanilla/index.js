b9483d7e33bef7e3dde1503cf1022a62d7bcbcb7
habib27akbar <akbarca94@gmail.com>
2021-02-05 21:06:05 +0700

    belajar sebentar :)

--- src/pages/CallAPIVanilla/index.js
+++ src/pages/CallAPIVanilla/index.js
@@ -1,32 +1,37 @@
 import React, { useEffect } from 'react';
-import {Image, StyleSheet, Text, View} from 'react-native';
+import {Button, StyleSheet, Text, View} from 'react-native';
 
 const CallAPIVanilla = () => {
     useEffect (() => {
         //Call API method GET
+        // fetch('https://jsonplaceholder.typicode.com/todos/1')
+        // .then(response => response.json())
+        // .then(json => console.log(json))
+
+        //Call API method POST
+        // const dataForAPI = {
+            
+        //         name: "morpheus",
+        //         job: "leader"
+            
+        // }
+        // fetch('https://reqres.in/api/users', {
+        //     method: 'POST',
+        //     headers: {
+        //         'Content-Type': 'application/json'
+        //     },
+        //     body: JSON.stringify(dataForAPI)
+        // })
+        // .then(response => response.json())
+        // .then(json => {
+        //     console.log('post response: ', json)
+        // })
+    }, []);
+    const getData = () => {
         fetch('https://jsonplaceholder.typicode.com/todos/1')
         .then(response => response.json())
         .then(json => console.log(json))
-
-        //Call API method POST
-        const dataForAPI = {
-            
-                name: "morpheus",
-                job: "leader"
-            
-        }
-        fetch('https://reqres.in/api/users', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(dataForAPI)
-        })
-        .then(response => response.json())
-        .then(json => {
-            console.log('post response: ', json)
-        })
-    }, []);
+    }
     return (
       <View style={styles.container}>
          
@@ -34,6 +39,11 @@ const CallAPIVanilla = () => {
             style={styles.text}>
             API React JS
           </Text>
+          <Button title="GET DATA" onPress={getData}/>
+          <Text style={styles.text}>Response GET DATA</Text>
+          <View style={styles.lines}/>
+          <Button title="POST DATA"/>
+          <Text style={styles.text}>Response POST DATA</Text>
           
           </View>
     );
@@ -44,6 +54,11 @@ const CallAPIVanilla = () => {
     },
     container: {
       padding: 20,
+    },
+    lines: {
+      height:2,
+      backgroundColor: 'black',
+      marginVertical: 20
     }
   })
   export default CallAPIVanilla;
