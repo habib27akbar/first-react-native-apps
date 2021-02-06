7885c3329731b0cc06076b043d74f33d0d89259f
habib27akbar <akbarca94@gmail.com>
2021-02-06 12:21:40 +0700

    belajar API get dan post data

--- src/pages/CallAPIVanilla/index.js
+++ src/pages/CallAPIVanilla/index.js
@@ -1,7 +1,17 @@
-import React, { useEffect } from 'react';
-import {Button, StyleSheet, Text, View} from 'react-native';
+import React, { useEffect, useState } from 'react';
+import {Button, Image, StyleSheet, Text, View} from 'react-native';
 
 const CallAPIVanilla = () => {
+  const [dataUser, setDataUser] = useState ({
+    email: '',
+    first_name: '',
+    last_name: '',
+    avatar: '',
+  })
+  const [dataJob, setDataJob] = useState ({
+    name: '',
+    job: '',
+  })
     useEffect (() => {
         //Call API method GET
         // fetch('https://jsonplaceholder.typicode.com/todos/1')
@@ -28,23 +38,51 @@ const CallAPIVanilla = () => {
         // })
     }, []);
     const getData = () => {
-        fetch('https://jsonplaceholder.typicode.com/todos/1')
+        fetch('https://reqres.in/api/users/2')
         .then(response => response.json())
-        .then(json => console.log(json))
+        .then(json => {
+          console.log(json)
+          setDataUser(json.data)
+        })
+    }
+    const postData = () => {
+        //Call API method POST
+        const dataForAPI = {
+            
+                name: "morpheus",
+                job: "leader"
+            
+        }
+        fetch('https://reqres.in/api/users', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(dataForAPI)
+        })
+        .then(response => response.json())
+        .then(json => {
+            console.log('post response: ', json)
+            setDataJob(json)
+        })
     }
     return (
       <View style={styles.container}>
          
           <Text 
             style={styles.text}>
-            API React JS
+            API  Vanilla JS
           </Text>
           <Button title="GET DATA" onPress={getData}/>
           <Text style={styles.text}>Response GET DATA</Text>
+          <Image source={{ uri: dataUser.avatar }}  style={{ width:100, height:100, borderRadius:50 }}/>
+          <Text>{`${dataUser.first_name} ${dataUser.last_name}`}</Text>
+          <Text>{`${dataUser.email}`}</Text>
           <View style={styles.lines}/>
-          <Button title="POST DATA"/>
+          <Button title="POST DATA" onPress={postData}/>
           <Text style={styles.text}>Response POST DATA</Text>
-          
+          <Text>{dataJob.name}</Text>
+          <Text>{dataJob.job}</Text>
           </View>
     );
   };
